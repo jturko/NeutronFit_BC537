bf32049e9bf3f738fc9a976d774874754f57e28c
Joseph Turko <jturko@nessa.physics.uoguelph.ca>
2017-06-01 12:12:49 -0400

    runSimAn.C script added

---
+++ runSimAn.C
@@ -0,0 +1,48 @@
+
+#include "Fitter.cc"
+
+void runSimAn()
+{
+    Fitter fit(50,37,20,15,18,58,56,52);    
+    
+    fit.SetSimAnHigh(   0,  0.92);      // a1
+    fit.SetSimAnLow(    0,  0.55);
+    fit.SetSimAnStep(   0,  0.005);
+    
+    fit.SetSimAnHigh(   1,  6.2);       // a2 
+    fit.SetSimAnLow(    1,  1.5);
+    fit.SetSimAnStep(   1,  0.005);
+    
+    fit.SetSimAnHigh(   2,  0.34);      // a3
+    fit.SetSimAnLow(    2,  0.13);
+    fit.SetSimAnStep(   2,  0.005);
+    
+    fit.SetSimAnHigh(   3,  1.05);      // a4
+    fit.SetSimAnLow(    3,  0.95);
+    fit.SetSimAnStep(   3,  0.005);
+    
+    fit.SetSimAnHigh(   4,  0.02);      // 12C
+    fit.SetSimAnLow(    4,  -0.0001);
+    fit.SetSimAnStep(   4,  0.0001);
+    
+    fit.SetSimAnHigh(   5,  0.25);      // A
+    fit.SetSimAnLow(    5,  0.10);
+    fit.SetSimAnStep(   5,  0.01);
+    
+    fit.SetSimAnHigh(   6,  0.25);      // B
+    fit.SetSimAnLow(    6,  0.10);
+    fit.SetSimAnStep(   6,  0.01);
+    
+    fit.SetSimAnHigh(   7,  0.01);      // C
+    fit.SetSimAnLow(    7,  0.0001);
+    fit.SetSimAnStep(   7,  0.0001);
+
+    fit.SetInLoopMax(50);
+    
+    double T_i = 100;
+    double T_f = 0.01;
+    double alpha = 0.98;
+
+    fit.MyMinimizeSimAn(alpha,T_i,T_f);
+
+}
