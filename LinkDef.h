c733212e1ffc9c111c3a9fe2d53a8e887bf2fc15
jturko <JTurko@Joeys-MacBook-Pro-2.local>
2017-06-29 15:57:44 -0400

    whoops, forgot the LinkDef.h needed to compile the .so library

---
+++ LinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class NeutronFit_BC537+;
+#pragma link C++ class Fitter+;
+
+#pragma link C++ defined_in "NeutronFit_BC537.hh";
+#pragma link C++ defined_in "Fitter.hh";
+
+#endif
