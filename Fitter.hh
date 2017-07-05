fb4130a0ca082753e747ab06a615d6e8fc348cc8
Joey Turko <jturko@mail.uoguelph.ca>
2017-07-05 16:11:09 -0400

    Run9() function added for output from MyMinimizeSimAn9() method

--- Fitter.hh
+++ Fitter.hh
@@ -54,6 +54,10 @@ public:
 
     void Draw();
     void Run(double a1=0.639, double a2=1.462, double a3=0.373, double a4=0.968, double carbon=0, double A=0.123, double B=0.125, double C=0.0074);
+    void Run9(double a1=0.639, double a2=1.462, double a3=0.373, double a4=0.968, double carbon=0, double A=0.123, double B=0.125, double C=0.0074, double offset=0) {
+        SetOffset(offset);
+        Run(a1,a2,a3,a4,carbon,A,B,C);
+    }
     void RunOffset(double a1, double a2, double a3, double a4, double offset);
 
     bool Check(int i) { if(i<=-1||i>=GetNumberOfNeutronFit_BC537s()) return false; else return true; }
