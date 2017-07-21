91976ec4ce988e8fe4ace03eff0dc24b30313d34
Joseph Turko <jturko@eonwe.physics.uoguelph.ca>
2017-07-21 15:14:35 -0400

    using the plotEdgeFit script ( ~/data/testcan_neutron_raw/ ), got some a_i and A,B,C coefficients to try with the simulated annealing method, it looks like some of the variable limits were not that great...

--- fit.cc
+++ fit.cc
@@ -6,23 +6,33 @@
 int main()
 {
     //Fitter fit(50,37,20,15,18,58,56,52);    
-    Fitter fit(51, 46, 38, 2, 25, 4, 18, 58, 55 ,52);
-    
+    //Fitter fit(51, 46, 38, 2, 25, 4, 18, 58, 55 ,52);
+    Fitter fit(50, 46, 38, 2, 25, 4, 18, 58, 55 ,52);
+    fit.SetNextNeutronFit_BC537(49);
+    fit.SetNextNeutronFit_BC537(48);
+    fit.SetNextNeutronFit_BC537(47);
+   
     fit.SetMT(true);
    
+    fit.SetChi2Method(2);
+
+    //fit.SetSimSortMax(1e5);
+    fit.SetSimSortMax(1e5);
+    fit.PrintSimSortMax();
+
     fit.SetSimAnHigh(   0,  0.92);      // a1
-    fit.SetSimAnLow(    0,  0.55);
+    fit.SetSimAnLow(    0,  0.15);
     fit.SetSimAnStep(   0,  0.005);
     
     fit.SetSimAnHigh(   1,  6.2);       // a2 
-    fit.SetSimAnLow(    1,  1.5);
-    fit.SetSimAnStep(   1,  0.005);
+    fit.SetSimAnLow(    1,  0.1);
+    fit.SetSimAnStep(   1,  0.02);
     
-    fit.SetSimAnHigh(   2,  0.34);      // a3
+    fit.SetSimAnHigh(   2,  0.9);      // a3
     fit.SetSimAnLow(    2,  0.13);
     fit.SetSimAnStep(   2,  0.005);
     
-    fit.SetSimAnHigh(   3,  1.05);      // a4
+    fit.SetSimAnHigh(   3,  1.3);      // a4
     fit.SetSimAnLow(    3,  0.95);
     fit.SetSimAnStep(   3,  0.005);
     
@@ -30,16 +40,16 @@ int main()
     fit.SetSimAnLow(    4,  -0.0001);
     fit.SetSimAnStep(   4,  0.0001);
     
-    fit.SetSimAnHigh(   5,  0.25);      // A
-    fit.SetSimAnLow(    5,  0.10);
+    fit.SetSimAnHigh(   5,  0.4);      // A
+    fit.SetSimAnLow(    5,  0);
     fit.SetSimAnStep(   5,  0.01);
     
-    fit.SetSimAnHigh(   6,  0.25);      // B
-    fit.SetSimAnLow(    6,  0.10);
+    fit.SetSimAnHigh(   6,  0.4);      // B
+    fit.SetSimAnLow(    6,  0);
     fit.SetSimAnStep(   6,  0.01);
     
     fit.SetSimAnHigh(   7,  0.01);      // C
-    fit.SetSimAnLow(    7,  0.0001);
+    fit.SetSimAnLow(    7,  0);
     fit.SetSimAnStep(   7,  0.0001);
 
     fit.SetSimAnHighOffset(20);
@@ -53,9 +63,28 @@ int main()
 
     double T_i = 10;
     double T_f = 0.01;
-    double alpha = 0.995;
+    double alpha = 0.998;
 
-    fit.MyMinimizeSimAn9(alpha,T_i,T_f);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //fit.SetSimAnStart( 0.57381 , 1.58280 , 0.30884 , 0.99951 , -0.00006 , 0.24603 , 0.10532 , 0.00169 , 7.55915 );
+    //fit.SetInLoopMax(50);
+    //alpha = 0.999;
+    //T_i = 0.1;    
+    //fit.MyMinimizeSimAn9(alpha,T_i,T_f);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    fit.SetSimAnStart( 4.39550e-01 , 5.79582e-01 , 6.61679e-01 , 1.16190e+00 , 0, 0.284 , 0.064 , 0.00002 , 0); // values from fitting the 50%  edge PH
+    fit.SetOffset(0);
+    fit.SetInLoopMax(1);
+    alpha = 0.999;
+    T_i = 0.1;    
+    fit.MyMinimizeSimAn(alpha,T_i,T_f);
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    //fit.MyMinimizeSimAn9(alpha,T_i,T_f);
+    
+    //fit.SetOffset(4.81);
+    //fit.NelderMead(0.57791 , 1.71631 , 0.28050 , 0.99953 , -0.00002 , 0.24233 , 0.11107 , 0.00612);
 
     return 0;
 
