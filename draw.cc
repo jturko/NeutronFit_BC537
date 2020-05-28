bcb9e873a789e1bf6d2899f2488c183e00c3463b
jturko <jturko@mail.uoguelph.ca>
2020-05-28 11:24:55 -0400

    changes for the original binning

--- draw.cc
+++ draw.cc
@@ -8,53 +8,19 @@ int main()
     Fitter fit;
  
     fit.LoadAll(true);
-    //for(int i=0; i<51; i++) fit.SetNextNeutronFit_BC537(i);
-    //for(int i=52; i<64; i++) fit.SetNextNeutronFit_BC537(i);
 
     fit.SetSimSortMax(-1);    
     fit.SetChi2Method(2);
     
-    //fit.Print();
+    fit.SetParameters(4.39550e-01 , 5.79582e-01 , 6.61679e-01 , 1.16190e+00 , 0 , 0 , 0 , 0);
+    fit.SetOffset(0);
+    fit.SortAllRunsMT();
+    fit.WriteToFile("sorted_forpaper_nr.root");
 
-    //fit.SetParameters( 0.63150 , 2.32135 , 0.22820 , 0.99025 , -0.00008 , 0.24276 , 0.12991 , 0.00166);
-    //fit.SetOffset(5.20537);
-    
-    //fit.SetParameters( 0.57791 , 1.71631 , 0.28050 , 0.99953 , -0.00002 , 0.24233 , 0.11107 , 0.00612);
-    //fit.SetParameters( 0.57783 , 1.70985 , 0.282788 , 1.00048 , -8.32195e-05 , 0.259175 , 0.110136 , 0.00615189);
-    //fit.SetOffset(4.81);
-    
-    //fit.SetParameters(0.67065 , 2.84380 , 0.19805 , 1.00051 , 0.00356 , 0.22379 , 0.19175 , 0.00019);
-    //fit.SetOffset( 1.11618 );
-    
-    // these are the best parameters from fit_nessa.out
-    //fit.SetParameters( 0.57791 , 1.71631 , 0.28050 , 0.99953 , -0.00002 , 0.24233 , 0.11107 , 0.00612 );
-    //fit.SetOffset( 4.80967 );
-
-    // from fit_nessa3_moreLowEnergyRuns.out 
-    //fit.SetParameters( 0.56282 , 1.64118 , 0.28513 , 0.98436 , -0.00010 , 0.24314 , 0.10446 , 0.00141 );
-    //fit.SetParameters( 0.56282 , 1.64118 , 0.28513 , 0.98436 , -0.00010 , 0, 0, 0);
-    //fit.SetOffset( 8.20668 );
-
-    // NelderMead output from best result in fit_nessa3_moreLowEnergyRuns.out
-    //fit.SetParameters(0.571959 , 1.58203 , 0.307729 , 0.998759 , -4.90761e-05 , 0.251424 , 0.100439 , 0.00175765);
-    //fit.SetOffset(7.61182);
-
-    // fit parameters from plotEdgeFit script output + fit
-    //fit.SetParameters(4.39550e-01 , 5.79582e-01 , 6.61679e-01 , 1.16190e+00 , 0 , 0 , 0 , 0);
     fit.SetParameters(4.39550e-01 , 5.79582e-01 , 6.61679e-01 , 1.16190e+00 , 0, 0.284 , 0.064 , 0.00002);
     fit.SetOffset(0);
-
-
-    // massive negative overall chi2 results from fit_nessa3_try3...out
-    //fit.SetParameters(0.55917 , 1.64428 , 0.29104 , 1.00054 , 0.00006 , 0.24106 , 0.12978 , 0.00912);
-    //fit.SetOffset( 6.16387 );
-
     fit.SortAllRunsMT();
-    
-    fit.PrintChi2();
-
-    fit.DrawToFile("draw.pdf");
-
+    fit.WriteToFile("sorted_forpaper.root");
     return 0;
 
 }
